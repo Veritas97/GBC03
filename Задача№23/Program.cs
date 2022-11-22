06057119037f45104b44cf4d38cf36f517159b20
Veritas97 <lykovcl@gmail.com>
2022-11-22 18:13:18 +0900

    Good Task23

---
+++ Задача№23/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число N: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+
+void square (int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+       Console.WriteLine($"{i} | {i*i*i}");
+    }
+}
+square(numN);
