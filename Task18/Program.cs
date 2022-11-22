a0076c803e810ec347f7459b1f248432a14c4ce4
Veritas97 <lykovcl@gmail.com>
2022-11-22 16:39:20 +0900

    Good Task18

---
+++ Task18/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите кординаты: ");
+int numquar = Convert.ToInt32(Console.ReadLine());
+
+string Range(int quar)
+{
+    if (quar==1) return "(x > 0 и y > 0)";
+    if (quar==2) return "(x < 0 и y > 0)";
+    if (quar==3) return "(x < 0 и y < 0)";
+    if (quar==4) return "(x > 0 и y < 0)";
+    return "Введен неректный диапозон";
+}
+string range = Range(numquar);
+
+Console.Write($"Кординаты в указоной четверти -- {range}");
